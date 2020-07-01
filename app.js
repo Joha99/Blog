e1a8a9dcefca37a2e88cdbec92c9a59c0cb70649
Joha Kim <johakim9936@gmail.com>
2020-07-01 08:06:25 -0400

    use lodash library to format strings

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash"); 
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -51,22 +52,17 @@ app.post("/compose", function (req, res) {
 
 // GET /posts
 // using express routing parameters 
+// use lodash to format string
 app.get("/posts/:postID", function (req, res) {
-
-  let postID = req.params.postID; 
-  let titleFound = false; 
+   
+  let postID = _.lowerCase(req.params.postID); 
 
   posts.forEach(function (post) {
-    if (post.title == postID) {
-      titleFound = true; 
+    let postTitle = _.lowerCase(post.title); 
+    if (postTitle === postID) {
+      console.log("Title match found"); 
     }
   }); 
-  
-  if (titleFound) {
-    console.log("Title match found"); 
-  } else {
-    console.log("Title match NOT FOUND"); 
-  }
 }); 
 
 
